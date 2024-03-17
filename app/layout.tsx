e5195170f9a3d641a3e80b32d7fd582f9c869ee2
Neamul01 <neamulhaque2002@gmail.com>
2024-03-18 03:50:49 +0600

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/MUI/Navbar";
+import Footer from "@/components/layout/Footer";
 // import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/layout/Footer.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+  FaTwitter,
+  FaWhatsapp,
+  FaYoutube,
+} from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <div className="bg-black text-white">
+      <div className="max-w-sectionLayout mx-auto py-12 flex justify-between">
+        <div className="flex flex-col max-w-[45%]">
+          <p className="font-bold border-b-[1px] border-gray-500 mb-4 pb-2">
+            About us
+          </p>
+          <div className="flex flex-col gap-2">
+            <p className="text-white/80 text-sm">
+              Kubernetes Community Days Kerala (KCD Kerala) is a
+              community-organized event supported by the Cloud Native Computing
+              Foundation (CNCF) to help grow and sustain the Kubernetes and
+              cloud-native community. It gathers adopters and technologists from
+              open source and cloud-native communities for education,
+              collaboration and networking.
+            </p>
+            <p className="text-white/80 text-sm">
+              Kerala has a growing group of Cloud Native enthusiasts, students,
+              professionals and technology leaders. KCD Kerala offers a platform
+              for this community to come together and connect with other Cloud
+              Native communities in India and neighbouring countries. It
+              provides an opportunity to experience conferences like KubeCon /
+              CloudNativeCon together with the rich cultural heritage of Kerala.
+              We are excited and looking forward to building a thriving Cloud
+              Native hub in Kerala.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col max-w-[30%] ">
+          <p className="font-bold border-b-[1px] border-gray-500 mb-4 pb-2">
+            Connect with us
+          </p>
+          <div className="flex items-center gap-3">
+            <a href="#">
+              <FaLinkedinIn
+                size={25}
+                className="text-[#007bff] hover:text-white"
+              />
+            </a>
+            <a href="#">
+              <FaFacebookF
+                size={25}
+                className="text-[#007bff] hover:text-white"
+              />
+            </a>
+            <a href="#">
+              <FaWhatsapp
+                size={25}
+                className="text-[#007bff] hover:text-white"
+              />
+            </a>
+            <a href="#">
+              <FaInstagram
+                size={25}
+                className="text-[#007bff] hover:text-white"
+              />
+            </a>
+            <a href="#">
+              <FaTwitter
+                size={25}
+                className="text-[#007bff] hover:text-white"
+              />
+            </a>
+            <a href="#">
+              <FaYoutube
+                size={25}
+                className="text-[#007bff] hover:text-white"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="py-3">
+        <p className="text-center text-sm text-white/60">
+          &copy; 2024 All Rights Reserved &copy; KCD Dhaka
+        </p>
+      </div>
+    </div>
+  );
+}
