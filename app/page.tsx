efd17e6c83fc13ff99e7f5bb7ba6855420584f7f
Neamul01 <neamulhaque2002@gmail.com>
2024-03-18 01:43:25 +0600

    add Venue and update hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
 import Hero from "@/components/Hero";
 import Schedule from "@/components/Schedule";
 import Speakers from "@/components/Speakers";
+import Venue from "@/components/Venue";
 import Welcome from "@/components/Welcome";
 import MainLayout from "@/components/layout/MainLayout";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
+      <Hero />
       <MainLayout>
-        <Hero />
         <Welcome />
       </MainLayout>
       <Speakers />
       <MainLayout>
         <Schedule />
       </MainLayout>
+      <Venue />
     </main>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,13 +2,25 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="text-secondary">
-      <div className="flex flex-col">
-        <h2 className="text-[35px] font-semibold ">KCD Kerala 2024</h2>
-        <p className="font-semibold text-[20px] ">3rd Feb, Le Meridien Kochi</p>
+    <div className="bg-[url('/b5.jpg')] bg-cover bg-no-repeat bg-center h-[1000px] -mt-[125px] ">
+      <div className="h-full bg-gradient-to-t from-black/75 to-black/70 flex items-center justify-start">
+        <div className="w-sectionLayout mx-auto">
+          <div className="flex flex-col mb-8">
+            <h2 className="text-[65px] text-secondary font-semibold ">
+              KCD Kerala 2024
+            </h2>
+            <p className="font-semibold text-[25px] text-white">
+              3rd Feb, Le Meridien Kochi
+            </p>
+          </div>
+          <p className="text-[35px] uppercase font-semibold text-secondary ">
+            TODAY..
+          </p>
+          <button className="text-white bg-primary px-8 py-4 rounded-lg mt-16">
+            Register Now
+          </button>
+        </div>
       </div>
-      <p className="text-[25px] uppercase font-semibold ">TODAY..</p>
-      <button>Register Now</button>
     </div>
   );
 }
