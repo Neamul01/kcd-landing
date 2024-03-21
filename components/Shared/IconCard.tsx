094fd91fbcc6f002c21fb57e6662bf5351236b64
Neamul01 <neamulhaque2002@gmail.com>
2024-03-22 03:51:35 +0600

    update sponsors

--- components/Shared/IconCard.tsx
+++ components/Shared/IconCard.tsx
@@ -9,7 +9,7 @@ type Sponsor = {
 
 export default function IconCard({ sponsor }: { sponsor: Sponsor }) {
   return (
-    <div className="w-[280px] h-[160px] p-5 rounded-lg bg-white border-4 border-gray-300">
+    <div className="w-[280px] h-[160px] p-5 rounded-lg bg-white border-4 border-gray-300 flex items-center justify-center">
       <Image src={sponsor.icon} alt="sponsors" height={111} width={240} />
     </div>
   );
--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -11,41 +11,76 @@ export default function Sponsors() {
     >
       <div className="h-full bg-gradient-to-t from-black/50 to-black/50 text-white">
         <SectionLayout title={"Sponsors & Partners"}>
-          <div className="max-w-sectionLayout mx-auto mt-6 md:mt-12">
-            <div className="flex flex-wrap items-center justify-center gap-x-10 gap-y-5">
-              {sponsors.map((sponsor) => (
-                <IconCard key={sponsor.id} sponsor={sponsor} />
-              ))}
+          <>
+            <div className="max-w-sectionLayout mx-auto mt-6 md:mt-12">
+              <div className="flex items-center justify-center mb-6">
+                <h2 className="flex flex-col items-center justify-center gap-1 font-semibold text-center">
+                  <span className="text-2xl uppercase ">Platinum</span>
+                  <span className="w-1/2 h-[2px] rounded-xl bg-secondary" />
+                </h2>
+              </div>
+              <div className="flex flex-wrap items-center justify-center gap-x-10 gap-y-5">
+                {platinumSponsors.length > 0 &&
+                  platinumSponsors.map((sponsor) => (
+                    <>
+                      <IconCard key={sponsor.id} sponsor={sponsor} />
+                    </>
+                  ))}
+              </div>
             </div>
-          </div>
+            {/* -------------GOLD--------- */}
+            <div className="max-w-sectionLayout mx-auto mt-6 md:mt-12">
+              <div className="flex items-center justify-center mb-6">
+                <h2 className="flex flex-col items-center justify-center gap-1 font-semibold text-center">
+                  <span className="text-2xl uppercase ">GOLD</span>
+                  <span className="w-1/2 h-[2px] rounded-xl bg-secondary" />
+                </h2>
+              </div>
+              <div className="flex flex-wrap items-center justify-center gap-x-10 gap-y-5">
+                {goldSponsors.length > 0 &&
+                  goldSponsors.map((sponsor) => (
+                    <>
+                      <IconCard key={sponsor.id} sponsor={sponsor} />
+                    </>
+                  ))}
+              </div>
+            </div>
+            {/* -------------SILVER------------ */}
+            <div className="max-w-sectionLayout mx-auto mt-6 md:mt-12">
+              <div className="flex items-center justify-center mb-6">
+                <h2 className="flex flex-col items-center justify-center gap-1 font-semibold text-center">
+                  <span className="text-2xl uppercase ">SILVER</span>
+                  <span className="w-1/2 h-[2px] rounded-xl bg-secondary" />
+                </h2>
+              </div>
+              <div className="flex flex-wrap items-center justify-center gap-x-10 gap-y-5">
+                {silverSponsors.length > 0 &&
+                  silverSponsors.map((sponsor) => (
+                    <>
+                      <IconCard key={sponsor.id} sponsor={sponsor} />
+                    </>
+                  ))}
+              </div>
+            </div>
+            {/* -------------BRONZE------------ */}
+            <div className="max-w-sectionLayout mx-auto mt-6 md:mt-12">
+              <div className="flex items-center justify-center mb-6">
+                <h2 className="flex flex-col items-center justify-center gap-1 font-semibold text-center">
+                  <span className="text-2xl uppercase ">BRONZE</span>
+                  <span className="w-1/2 h-[2px] rounded-xl bg-secondary" />
+                </h2>
+              </div>
+              <div className="flex flex-wrap items-center justify-center gap-x-10 gap-y-5">
+                {bronzeSponsors.length > 0 &&
+                  bronzeSponsors.map((sponsor) => (
+                    <>
+                      <IconCard key={sponsor.id} sponsor={sponsor} />
+                    </>
+                  ))}
+              </div>
+            </div>
+          </>
         </SectionLayout>
-
-        {/* <SectionLayout title={"Scholarship Sponsors"}>
-          <div className="max-w-sectionLayout mx-auto mt-6 md:mt-12">
-            <div className="flex flex-wrap items-center justify-center gap-x-10 gap-y-5">
-              {Scholarship.map((sponsor) => (
-                <div
-                  className="bg-transparent w-[290px] h-[354px] px-4 flex items-center justify-between flex-col"
-                  key={sponsor.id}
-                >
-                  <p className="text-secondary font-semibold text-center">
-                    {sponsor.title}
-                  </p>
-                  <Image
-                    alt="sponsor"
-                    src={sponsor.imageUrl}
-                    height={216}
-                    width={160}
-                    className="rounded-full"
-                  />
-                  <p className="font-bold text-sm">{sponsor.name} </p>
-                  <p className="text-base">{sponsor.designation} </p>
-                  <p className="text-xs">{sponsor.position} </p>
-                </div>
-              ))}
-            </div>
-          </div>
-        </SectionLayout> */}
 
         <SectionLayout title={"Community Partners"}>
           <div className="max-w-sectionLayout mx-auto mt-6 md:mt-12">
@@ -70,41 +105,45 @@ export default function Sponsors() {
   );
 }
 
-const sponsors = [
+const silverSponsors = [
+  {
+    id: "1",
+    name: "redhat",
+    icon: "/sponsors/apps-code.png",
+  },
+];
+
+const goldSponsors = [
+  {
+    id: "1",
+    name: "redhat",
+    icon: "/sponsors/placeholder.png",
+  },
+];
+
+const bronzeSponsors = [
+  {
+    id: "1",
+    name: "redhat",
+    icon: "/sponsors/placeholder.png",
+  },
+];
+
+const platinumSponsors = [
   {
     id: "1",
     name: "24_7",
-    icon: "/sponsors/24_7.png",
+    icon: "/sponsors/AES.png",
   },
   {
     id: "2",
-    name: "redhat",
-    icon: "/sponsors/redhat.png",
+    name: "prodevans",
+    icon: "/sponsors/astha.png",
   },
   {
     id: "3",
     name: "prodevans",
-    icon: "/sponsors/prodevans.png",
-  },
-  {
-    id: "4",
-    name: "kodekloud",
-    icon: "/sponsors/kodekloud.png",
-  },
-  {
-    id: "5",
-    name: "federalbank",
-    icon: "/sponsors/federalbank.png",
-  },
-  {
-    id: "6",
-    name: "flexicloud",
-    icon: "/sponsors/flexicloud.png",
-  },
-  {
-    id: "7",
-    name: "rezoomex",
-    icon: "/sponsors/rezoomex.png",
+    icon: "/sponsors/klover.png",
   },
 ];
 
