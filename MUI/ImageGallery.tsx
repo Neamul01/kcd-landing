147d93e8669f03be2935ceee59f762a9ef74a3f9
Neamul01 <neamulhaque2002@gmail.com>
2024-03-22 01:53:39 +0600

    update image galary

--- MUI/ImageGallery.tsx
+++ MUI/ImageGallery.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
+import Image from "next/image";
 
 export default function ImageGallery() {
   return (
@@ -9,8 +10,9 @@ export default function ImageGallery() {
       <ImageList variant="masonry" cols={3} gap={8}>
         {itemData.map((item) => (
           <ImageListItem key={item.img}>
-            <img
-              //   srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            <Image
+              height={233}
+              width={350}
               src={`${item.img}`}
               alt={item.title}
               loading="lazy"
@@ -24,39 +26,27 @@ export default function ImageGallery() {
 
 const itemData = [
   {
-    img: "https://kcdkerala.in/images/venue/l14.jpeg",
+    img: "https://www.bracinn.com/images/homeslides/slide04.jpg",
     title: "Bed",
   },
   {
-    img: "https://kcdkerala.in/images/venue/l1.jpeg",
-    title: "Books",
-  },
-  {
-    img: "https://kcdkerala.in/images/venue/l2.jpeg",
+    img: "https://www.bracinn.com/images/meetings/auditorium.jpg",
     title: "Sink",
   },
   {
-    img: "https://kcdkerala.in/images/venue/l3.jpeg",
+    img: "https://www.bracinn.com/images/meetings/meet02.jpg",
     title: "Kitchen",
   },
   {
-    img: "https://kcdkerala.in/images/venue/l4.jpeg",
+    img: "https://www.bracinn.com/images/homeslides/slide07.jpg",
     title: "Blinds",
   },
   {
-    img: "https://kcdkerala.in/images/venue/l5.jpeg",
+    img: "https://www.bracinn.com/images/homeslides/slide06.jpg",
     title: "Chairs",
   },
   {
-    img: "https://kcdkerala.in/images/venue/l8.jpeg",
-    title: "Laptop",
-  },
-  {
-    img: "https://kcdkerala.in/images/venue/l7.jpeg",
-    title: "Doors",
-  },
-  {
-    img: "https://kcdkerala.in/images/venue/l6.jpeg",
-    title: "Coffee",
+    img: "https://www.bracinn.com/images/homeslides/slide08.jpg",
+    title: "Books",
   },
 ];
