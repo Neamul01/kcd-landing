e90c6eb1dd4a88025551a67c5a41de2ceab26c15
Neamul01 <neamulhaque2002@gmail.com>
2024-03-23 15:58:39 +0600

    update hero height

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,9 +6,9 @@ function Hero() {
     <div className="bg-[url('/b5.jpg')] bg-cover bg-no-repeat bg-center h-[1000px] -mt-[125px] ">
       <div className="h-full bg-gradient-to-t from-black/75 to-black/70 flex items-center justify-start px-2">
         <div className="w-sectionLayout mx-auto">
-          <div className="flex flex-col mb-8">
-            <h2 className="text-[65px] text-secondary font-semibold ">
-              Dhaka Welcomes the Future of Tech KCD 2024!
+          <div className="flex flex-col mb-8 gap-9">
+            <h2 className="text-[65px] text-secondary font-semibold leading-tight tracking-wide">
+              Dhaka Welcomes the <br /> Future of Tech <br /> KCD 2024!
             </h2>
             <p className="font-semibold text-[25px] text-white">
               11th May, Brac Centre Inn.
@@ -17,12 +17,14 @@ function Hero() {
           {/* <p className="text-[35px] uppercase font-semibold text-secondary ">
             TODAY..
           </p> */}
-          <Link
-            href={"#buy-ticket"}
-            className="text-white bg-primary px-8 py-4 rounded-lg mt-16"
-          >
-            Register Now
-          </Link>
+          <div className="mt-16">
+            <Link
+              href={"#buy-ticket"}
+              className="text-white bg-primary px-8 py-4 rounded-lg "
+            >
+              Register Now
+            </Link>
+          </div>
         </div>
       </div>
     </div>
