d5be9f529476d477c5deb10400bfc835494747a4
Neamul01 <neamulhaque2002@gmail.com>
2024-03-26 04:40:03 +0600

    update footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 import { FaFacebookF, FaLinkedinIn, FaSlack, FaTwitter } from "react-icons/fa";
@@ -34,47 +35,66 @@ export default function Footer() {
               <p className="font-bold border-b-[1px] border-gray-500 mb-4 pb-2">
                 Connect with us
               </p>
-              <div className="flex items-center gap-3">
-                <a
-                  href="https://www.linkedin.com/company/kcd-dhaka/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaLinkedinIn
-                    size={25}
-                    className="text-[#007bff] hover:text-white"
-                  />
-                </a>
-                <a
-                  href="https://www.facebook.com/kcddhaka/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaFacebookF
-                    size={25}
-                    className="text-[#007bff] hover:text-white"
-                  />
-                </a>
-                <a
-                  href="https://twitter.com/i/flow/login?redirect_after_login=%2Fkcddhaka"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaTwitter
-                    size={25}
-                    className="text-[#007bff] hover:text-white"
-                  />
-                </a>
-                <a
-                  href="https://cloud-native.slack.com/?redir=%2Farchives%2FC06KC3Z9V16%3Fname%3DC06KC3Z9V16"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaSlack
-                    size={25}
-                    className="text-[#007bff] hover:text-white"
-                  />
-                </a>
+              <div className="flex flex-col gap-2 h-full justify-between">
+                <div className="flex flex-col gap-2">
+                  <p className="text-sm text-white">
+                    <a href="mailto:organizers@kcddhaka.org">
+                      organizers@kcddhaka.org
+                    </a>
+                  </p>
+                  <p className="text-sm text-white underline">
+                    <Link href={"/conditions/refund-policy"}>
+                      Refund policy
+                    </Link>
+                  </p>
+                  <p className="text-sm text-white underline">
+                    <Link href={"/conditions/terms-condition"}>
+                      Terms and Conditions
+                    </Link>
+                  </p>
+                </div>
+                <div className="flex items-center gap-3">
+                  <a
+                    href="https://www.linkedin.com/company/kcd-dhaka/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaLinkedinIn
+                      size={25}
+                      className="text-[#007bff] hover:text-white"
+                    />
+                  </a>
+                  <a
+                    href="https://www.facebook.com/kcddhaka/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaFacebookF
+                      size={25}
+                      className="text-[#007bff] hover:text-white"
+                    />
+                  </a>
+                  <a
+                    href="https://twitter.com/i/flow/login?redirect_after_login=%2Fkcddhaka"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaTwitter
+                      size={25}
+                      className="text-[#007bff] hover:text-white"
+                    />
+                  </a>
+                  <a
+                    href="https://cloud-native.slack.com/?redir=%2Farchives%2FC06KC3Z9V16%3Fname%3DC06KC3Z9V16"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaSlack
+                      size={25}
+                      className="text-[#007bff] hover:text-white"
+                    />
+                  </a>
+                </div>
               </div>
             </div>
           </div>
