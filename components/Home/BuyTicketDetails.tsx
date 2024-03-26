82de0e491b82b5728b3f53a49b4f6c4f13469c3f
Neamul01 <neamulhaque2002@gmail.com>
2024-03-27 00:38:10 +0600

    finish workshop form

--- components/Home/BuyTicketDetails.tsx
+++ components/Home/BuyTicketDetails.tsx
@@ -12,26 +12,34 @@ import Typography from "@mui/material/Typography";
 import { IoMdEye as EyeIcon, IoMdEyeOff as EyeSlashIcon } from "react-icons/io";
 import { Controller, useForm } from "react-hook-form";
 import { z as zod } from "zod";
-import { Order, Ticket } from "@/types/types";
+import { Order, Ticket, Workshop } from "@/types/types";
 import { useUser } from "@/hooks/use-user";
-import { Checkbox, FormControlLabel, MenuItem, Select } from "@mui/material";
+import {
+  Checkbox,
+  FormControlLabel,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+} from "@mui/material";
 import Link from "next/link";
+import axiosInstance from "@/lib/Axios";
 
 const schema = zod.object({
   email: zod.string().min(1, { message: "Email is required" }).email(),
   name: zod.string().min(1, { message: "Name is required" }),
   mobile: zod.string().min(1, { message: "Mobile number is required" }),
   address: zod.string().min(1, { message: "Address is required" }),
-  // designation: zod.string().min(1, { message: "Designation is required" }),
-  // organization: zod.string().min(1, { message: "Organization is required" }),
   tShirt: zod.string().min(1, { message: "Organization is required" }),
-  promotion: zod
-    .boolean()
-    .refine((value) => value, "You must accept the terms and conditions")
-    .optional(),
+  promotion: zod.boolean().optional(),
   terms: zod
     .boolean()
     .refine((value) => value, "You must accept the terms and conditions"),
+  track: zod
+    .string()
+    .min(1, { message: "Track selection is required" })
+    .optional(),
+  workshop: zod.array(zod.string()).optional(),
 });
 
 type Values = zod.infer<typeof schema>;
@@ -48,12 +56,14 @@ export default function BuyTicketDetails({
   const { data: user } = useUser();
 
   const [isPending, setIsPending] = React.useState<boolean>(false);
+  const [workshops, setWorkshops] = React.useState<Workshop[]>([]);
+  const [selectedWorkshop, setSelectedWorkspace] = React.useState<string[]>([]);
 
   const {
     control,
     handleSubmit,
-    setError,
     reset,
+    setValue,
     formState: { errors },
   } = useForm<Values>({
     defaultValues: React.useMemo(() => {
@@ -65,6 +75,7 @@ export default function BuyTicketDetails({
         tShirt: "",
         terms: false,
         promotion: false,
+        track: "",
       };
     }, [user]),
     resolver: zodResolver(schema),
@@ -82,13 +93,10 @@ export default function BuyTicketDetails({
             number: values.mobile, //
             promotion: values.promotion, //
           },
-          // track: values.
-          // workshop:
+          track: values.track,
+          workshop: selectedWorkshop,
           tshirt: values.tShirt, //
-          // description:
           address: values.address, //
-          // tax:
-          // shippingFee:
           cartItems: [
             {
               title: selectedTickets?.title, //
@@ -98,6 +106,8 @@ export default function BuyTicketDetails({
             },
           ],
         };
+
+        console.log("values", submitData);
 
         // Reset form
         reset();
@@ -109,6 +119,44 @@ export default function BuyTicketDetails({
     },
     [setOrderDetails, reset]
   );
+
+  const handleTrackSelection = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const selectedTrack = event.target.value;
+
+    setValue("track", selectedTrack);
+
+    if (selectedTrack === "workshop") {
+      try {
+        const response = await axiosInstance("/workshops");
+        if (!response.data) {
+          throw new Error("Failed to fetch workshops");
+        }
+        console.log("workshops", response.data.data);
+
+        setWorkshops(response.data.data);
+      } catch (error) {
+        console.error("Error fetching workshops:", error);
+      }
+    } else {
+      // Reset workshops if "Presentation" is selected
+      setWorkshops([]);
+    }
+  };
+
+  const handleWorkshopSelect = (workshopId: string) => {
+    console.log("workshop id", workshopId);
+    setSelectedWorkspace((prevSelected) => {
+      if (prevSelected.includes(workshopId)) {
+        // Remove workshopId if already present
+        return prevSelected.filter((id) => id !== workshopId);
+      } else {
+        // Add workshopId if not present
+        return [...prevSelected, workshopId];
+      }
+    });
+  };
 
   React.useEffect(() => {
     reset({
@@ -236,6 +284,59 @@ export default function BuyTicketDetails({
             )}
           />
 
+          {/* Track selection */}
+          <FormControl error={Boolean(errors.track)}>
+            <RadioGroup
+              aria-label="track"
+              name="track"
+              onChange={handleTrackSelection}
+            >
+              <FormControlLabel
+                value="presentation-deck"
+                control={<Radio />}
+                label="Presentation"
+              />
+              <FormControlLabel
+                value="workshop"
+                control={<Radio />}
+                label="Workshop"
+              />
+            </RadioGroup>
+            {errors.track && (
+              <FormHelperText>{errors.track.message}</FormHelperText>
+            )}
+          </FormControl>
+
+          {/* Workshop selection */}
+          {workshops && workshops?.length > 0 && (
+            <FormControl>
+              <RadioGroup aria-label="workshop" name="workshop">
+                {workshops?.map((workshop) => (
+                  <div key={workshop._id}>
+                    <FormControlLabel
+                      onClick={() => handleWorkshopSelect(workshop._id)}
+                      control={
+                        <Checkbox
+                          checked={selectedWorkshop.includes(workshop._id)}
+                        />
+                      }
+                      label={workshop.title}
+                      value={workshop._id}
+                    />
+                    {errors.workshop ? (
+                      <FormHelperText error>
+                        {errors.workshop.message}
+                      </FormHelperText>
+                    ) : null}
+                  </div>
+                ))}
+              </RadioGroup>
+            </FormControl>
+            // {/* {errors.workshop && (
+            //   <FormHelperText>{errors.workshop.message}</FormHelperText>
+            // )} */}
+          )}
+
           <Controller
             control={control}
             name="terms"
@@ -262,6 +363,7 @@ export default function BuyTicketDetails({
             )}
           />
         </Stack>
+        <button type="submit">submit</button>
       </form>
     </Stack>
   );
