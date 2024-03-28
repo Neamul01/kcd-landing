00c5667978d8343996cfe9f58699eafb0b0f0a5f
Neamul01 <neamulhaque2002@gmail.com>
2024-03-29 04:40:16 +0600

    add error and not-found

---
+++ app/auth/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Error from "@/components/layout/Error";
+import React from "react";
+
+export default function error() {
+  return (
+    <div>
+      <Error />
+    </div>
+  );
+}
---
+++ app/auth/not-found.tsx
@@ -0,0 +1,10 @@
+import NotFound from "@/components/layout/NotFound";
+import React from "react";
+
+export default function notfound() {
+  return (
+    <div>
+      <NotFound />
+    </div>
+  );
+}
