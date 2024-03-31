dce8028fa69bd430030a1d24ba7a10367a497730
Neamul01 <neamulhaque2002@gmail.com>
2024-03-31 21:05:55 +0600

    add privacy policy

---
+++ app/conditions/privacy-policy/page.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="max-w-sectionLayout mx-auto my-40">
+      <h2 className=" border-b-2 pb-3 text-2xl font-semibold mb-8">
+        Privacy policy
+      </h2>
+      <article className="prose lg:prose-xl">
+        <p>
+          This Privacy Policy outlines how Kubernetes Community Day Dhaka
+          (referred to as "we," "us," or "our") collects, uses, maintains, and
+          discloses information collected from users (referred to as "you" or
+          "your") of our website and services. Collection and Use of Personal
+          Information:
+        </p>
+        <p>
+          <span className="font-semibold pr-3">1.1.</span>
+          We may collect personal information from users in various ways,
+          including but not limited to when users visit our website, register
+          for an event, fill out a form, or interact with our services. Personal
+          information may include, but is not limited to, name, email address,
+          contact number, and other relevant details. We collect this
+          information only if provided voluntarily by users.
+        </p>
+        <p>
+          <span className="font-semibold pr-3">1.2.</span>
+          We use the collected information for the following purposes: To
+          provide our services and communicate with users regarding their
+          registrations or inquiries. To personalise user experience and improve
+          our website and services. To send periodic emails regarding event
+          updates, promotions, or other relevant information. Users may opt-out
+          of receiving such communications at any time.
+        </p>
+        <p>
+          <span className="font-semibold pr-3">1.3.</span>
+          We are committed to protecting the privacy and security of your
+          personal information. We implement industry-standard security measures
+          to safeguard your data against unauthorised access, disclosure, or
+          alteration. Third-Party Services:
+        </p>
+        <p>
+          <span className="font-semibold pr-3">2.1.</span>
+          We may use third-party services to facilitate payments, enhance user
+          experience, or analyse website usage. These third-party services may
+          collect and process user information according to their own privacy
+          policies. We encourage users to review the privacy policies of these
+          third-party services before using them through our website.
+        </p>
+        <p>
+          <span className="font-semibold pr-3">3.1.</span>
+          Our website may use cookies and similar tracking technologies to
+          enhance user experience and analyse website usage. Users may choose to
+          set their web browsers to refuse cookies or alert them when cookies
+          are being sent. However, disabling cookies may affect the
+          functionality of certain parts of our website.
+        </p>
+        <div>
+          <h4>Compliance with Laws:</h4>
+          <p>
+            <span className="font-semibold pr-3">4.1.</span>
+            We comply with all applicable laws and regulations regarding the
+            collection, use, and disclosure of personal information, including
+            but not limited to laws regarding data protection and privacy.
+          </p>
+        </div>
+        <div>
+          <h4>Changes to this Privacy Policy:</h4>
+          <p>
+            <span className="font-semibold pr-3">5.1.</span>
+            We reserve the right to update or modify this Privacy Policy at any
+            time without prior notice. Any changes will be effective immediately
+            upon posting on this page. We encourage users to periodically review
+            this Privacy Policy for any updates.
+          </p>
+        </div>
+        <div>
+          <h4>Contact Us:</h4>
+          <p>
+            <span className="font-semibold pr-3">6.1.</span>
+            If you have any questions or concerns about this Privacy Policy or
+            our privacy practices, please contact us at organizers@kcddhaka.org.
+            By using our website and services, you acknowledge that you have
+            read, understood, and agree to be bound by this Privacy Policy.
+          </p>
+        </div>
+      </article>
+    </div>
+  );
+}
