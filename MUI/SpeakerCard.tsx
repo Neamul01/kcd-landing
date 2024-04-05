e487082bdf2396203f6a3b20022586564df31355
user.email <jobayer.ahmed109@gmail.com>
2024-04-05 16:51:40 +0600

    Displayed keynote and event speakers dynamically

--- MUI/SpeakerCard.tsx
+++ MUI/SpeakerCard.tsx
@@ -1,59 +1,71 @@
-import * as React from "react";
+import { CardActionArea } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
-import { FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import Image from "next/image";
+import { FaLinkedinIn } from "react-icons/fa";
 
 type Speaker = {
-  id: number;
+  _id: string;
   name: string;
-  industry: string;
-  position: string;
-  imageUrl: string;
+  organization: string;
+  designation: string;
+  photo: string;
+  sponsor_link: string;
 };
 export default function SpeakerCard({ speaker }: { speaker: Speaker }) {
   return (
     <Card
-      sx={{ maxWidth: 345 }}
-      className="bg-transparent shadow-none hover:bg-transparent mx-auto"
+      key={speaker._id}
+      sx={{ maxWidth: 355 }}
+      className="bg-transparent shadow-none hover:bg-transparent "
     >
-      <CardActionArea className="hover:bg-transparent pt-3">
-        <CardMedia
-          component="img"
-          className="rounded-xl bg-transparent w-[160px] h-[216px] mx-auto "
-          image={speaker.imageUrl}
-          alt="green iguana"
-        />
+      <CardActionArea className="hover:bg-transparent py-4 bg-transparent w-[300px] h-[394px] px-4 flex items-center justify-around flex-col ">
+        <div className="">
+          <Image
+            alt="Speaker"
+            src={
+              speaker.photo
+                ? `${process.env.NEXT_PUBLIC_BASE_URL}/${speaker.photo}`
+                : "/download.jpeg"
+            }
+            height={170}
+            width={160}
+            className="h-[170px] w-[160px] overflow-hidden rounded-full border border-gray-500"
+          />
+        </div>
         <CardContent className="max-w-[200px] px-1">
           <Typography
             gutterBottom
             variant="h5"
             component="p"
-            className="font-bold text-base text-center"
+            className="font-bold text-lg text-center"
           >
             {speaker.name}
           </Typography>
+
           <Typography
             variant="body2"
-            color="text.secondary"
-            className="text-center leading-8 text-base tracking-wider"
+            color="text.base"
+            className="text-base text-center"
           >
-            {speaker.industry}
+            {speaker.designation}
           </Typography>
           <Typography
             variant="body2"
             color="text.secondary"
-            className="text-center text-sm mt-3"
+            className="text-sm text-center"
+            // className="text-center leading-8 text-base tracking-wider font-semibold"
           >
-            {speaker.position}
+            {speaker.organization}
           </Typography>
           <a
             href="#"
             className="text-blue-600 flex items-center justify-center mt-2"
           >
-            <FaLinkedinIn size={25} />
+            <a href={speaker.sponsor_link} target="_blank">
+              <FaLinkedinIn size={25} />
+            </a>
           </a>
         </CardContent>
       </CardActionArea>
--- components/Speakers.tsx
+++ components/Speakers.tsx
@@ -1,18 +1,31 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import SectionLayout from "./layout/SectionLayout";
 import SpeakerCard from "@/MUI/SpeakerCard";
-import Axios from "@/lib/Axios";
+import axiosInstance from "@/lib/Axios";
+import { useEffect, useState } from "react";
+import Loader from "./Shared/Loader";
+import SectionLayout from "./layout/SectionLayout";
 
+type Speaker = {
+  _id: string;
+  name: string;
+  organization: string;
+  designation: string;
+  photo: string;
+};
 export default function Speakers() {
-  const [speakers, setSpeakers] = useState();
+  const [keynoteSpeakers, setKeyNoteSpeakers] = useState<Speaker[]>([]);
+  const [eventSpeakers, setEventSpeakers] = useState<Speaker[]>([]);
 
-  const getData = async () => {
+  console.log(eventSpeakers);
+
+  const getKeynoteSpeakers = async () => {
     try {
-      const response = await Axios.get("/participants?limit=10");
+      const response = await axiosInstance.get(
+        "/participants?role=key-note-speaker"
+      );
 
       // Handle the response data here
-      console.log(response.data);
+      setKeyNoteSpeakers(response.data.data);
     } catch (error) {
       // Handle the error here
       console.error(error);
@@ -20,36 +33,54 @@ export default function Speakers() {
   };
 
   useEffect(() => {
-    getData();
+    getKeynoteSpeakers();
+  }, []);
+
+  const getEventSpeakers = async () => {
+    try {
+      const response = await axiosInstance.get(
+        "/participants?role=event-speaker"
+      );
+
+      // Handle the response data here
+      setEventSpeakers(response.data.data);
+    } catch (error) {
+      // Handle the error here
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getEventSpeakers();
   }, []);
 
   return (
-    <div id="speakers">
-      <SectionLayout title={"Keynote Speakers"} className="bg-gray-200">
-        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-10 lg:gap-16 max-w-sectionLayout mx-auto ">
-          {keynoteSpeakers.map((speaker) => (
-            <SpeakerCard key={speaker.id} speaker={speaker} />
-          ))}
-        </div> */}
-        <p className="text-center text-primary w-full">
-          Keynote Speakers update coming soon. Stay tuned to our website for
-          details
-        </p>
+    <div id="speakers ">
+      <SectionLayout title={"Keynote Speakers"} className="">
+        {keynoteSpeakers.length ? (
+          <div className="flex flex-wrap items-center justify-center gap-y-20 md:gap-y-20 max-w-sectionLayout mx-auto mt-6 md:mt-12">
+            {keynoteSpeakers.map((speaker) => (
+              <SpeakerCard key={speaker._id} speaker={speaker} />
+            ))}
+          </div>
+        ) : (
+          <Loader />
+        )}
       </SectionLayout>
       <SectionLayout
         paddingBottom
         title={"Event Speakers"}
-        className="bg-gray-200"
+        className="max-w-sectionLayout mx-auto mt-6 md:mt-12"
       >
-        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-10 lg:gap-16 max-w-sectionLayout mx-auto">
-          {keynoteSpeakers.map((speaker) => (
-            <SpeakerCard key={speaker.id} speaker={speaker} />
-          ))}
-        </div> */}
-        <p className="text-center text-primary w-full">
-          Event Speakers update coming soon. Stay tuned to our website for
-          details
-        </p>
+        {eventSpeakers.length ? (
+          <div className="flex flex-wrap items-center justify-center gap-y-20 md:gap-y-20">
+            {eventSpeakers.map((speaker) => (
+              <SpeakerCard key={speaker._id} speaker={speaker} />
+            ))}
+          </div>
+        ) : (
+          <Loader />
+        )}
       </SectionLayout>
     </div>
   );
