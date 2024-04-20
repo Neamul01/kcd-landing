a2dd21c6f114357c1ee74843d4e8173eb64cd33c
Neamul01 <neamulhaque2002@gmail.com>
2024-04-21 01:47:18 +0600

    make volunteer component separate

---
+++ components/Volunteers.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import SectionLayout from "./layout/SectionLayout";
+
+export default function Volunteers() {
+  return (
+    <SectionLayout paddingBottom title={"Volunteers"}>
+      <div className="max-w-sectionLayout text-center text-black/90 mx-auto gap-2 flex flex-col">
+        {/* <p>
+            We are seeking dedicated and passionate individuals to join us as
+            volunteers.
+          </p>
+          <p>
+            Feel free to share your proposal with us by submitting it
+            <a href="#" className="text-primary underline pl-1">
+              here
+            </a>
+          </p> */}
+      </div>
+    </SectionLayout>
+  );
+}
